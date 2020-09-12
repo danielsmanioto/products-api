99ebc832734ad8ce17cdc98b43cfcd8c06282c2d
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-09-12 17:51:50 -0300

    gitignore and add UserApplication class

---
+++ src/main/java/com/dsmanioto/productsapi/model/UserApplication.java
@@ -0,0 +1,31 @@
+package com.dsmanioto.productsapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Document
+@Getter
+@Setter
+@EqualsAndHashCode
+public class UserApplication {
+
+    @Id
+    @Column(unique = true)
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    @JsonIgnore
+    private String password;
+
+    @NotEmpty
+    private String name;
+
+}
